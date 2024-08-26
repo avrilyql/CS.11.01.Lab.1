0fa723c9da22cddff7a66e5da5da1a5a539ae09c
avrilyql <avrilyql2027@student.cis.edu.hk>
2024-08-26 09:00:14 +0800

    Finished Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner inputAge = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int yourAge = inputAge.nextInt();
+        System.out.println(yourAge + " year olds should date somebody at least " + (7+(yourAge/2)) + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner inputAge = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int yourAge = inputAge.nextInt();
+        System.out.println(yourAge + " year olds should read at least " + (100-yourAge) + " pages of a book before giving up.");
     }
 }
